62f648fb37dd00eb57b4aa79fd78a138df8e8f0e
LovelySanta <laurens.scheldeman@gmail.com>
2018-07-05 15:02:58 +0200

    Fixed #1 green science require fast inserters when inserter overhaul is set to true

--- boblogistics/data-updates.lua
+++ boblogistics/data-updates.lua
@@ -13,8 +13,21 @@ if mods["boblogistics"] then
 
   MoreScience.lib.technology.addPrerequisite("logistics-3", "advanced-science-research-1")
   MoreScience.lib.technology.addIngredient("logistics-3", 1, "production-science-pack")
-  
+
   -- disable underground pipes
   MoreScience.lib.technology.removeRecipeUnlock("steel-processing", "steel-pipe-to-ground")
+
+  -- Fix the inserter overhaul
+  if settings.startup["bobmods-logistics-inserteroverhaul"] and settings.startup["bobmods-logistics-inserteroverhaul"].value == true then
+    -- unlock fast inserter for green science early game
+    MoreScience.lib.technology.removeRecipeUnlock("logistics-2", "long-handed-inserter")
+    MoreScience.lib.technology.addRecipeUnlock("automation-2", "long-handed-inserter")
+    -- change green science recipe over to fast inserter from express inserter
+    MoreScience.lib.recipe.editIngredient("basic-science-fluid-2", "fast-inserter", "long-handed-inserter", 1)
+
+    -- unlock filter inserter quicker
+    MoreScience.lib.technology.removeRecipeUnlock("logistics-2", "red-filter-inserter")
+    MoreScience.lib.technology.addRecipeUnlock("automation-2", "red-filter-inserter")
+  end
 
 end
